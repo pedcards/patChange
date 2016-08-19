da37f64775447d613bb46a09364adc3df1bb0f77
Terry Chun <docterry@hotmail.com>
2016-08-19 13:04:59 -0700

    Beginning logic for do=sync

--- direct.php
+++ direct.php
@@ -1,18 +1,21 @@
 <?php
-    $do = \filter_input(\INPUT_GET, 'do');
 
-    echo $do;
-    echo 'test';
+$do = \filter_input(\INPUT_GET, 'do');
+if ($do!=='sync') {
+    echo 'Error';
+    exit;
+}
+if (file_exists('change.xml')) {
+    $xml = simplexml_load_file('change.xml');
+} else {
+    echo 'NONE';
+    exit;
+    
+    $xml = new SimpleXMLElement('<root/>');
+    $xml->asXML("change.xml");
+}
 $timenow = date("YmdHis");
 $user = htmlentities($_SERVER['REMOTE_USER']);
 $refer = htmlentities($_SERVER['HTTP_REFERER']);
-file_put_contents("change".$timenow, $timenow.':'.$user.':'.$refer.':'.$do);
+//file_put_contents("change".$timenow, $timenow.':'.$user.':'.$refer.':'.$do);
 
-$xml = new DOMDocument;
-$xml_root = $xml->createElement("root");
-$xml_lvl1 = $xml->createElement("level 1");
-$xml->appendChild($xml_root);
-$xml_el = $xml_root->createElement("level 1");
-$xml_root->appendChild($xml_el);
-
-$xml->save("change.xml");
