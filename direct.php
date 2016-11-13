5de98ba5de63d908d7ac25b9d3ca3e2a6d8d9aa1
Terry Chun <docterry@hotmail.com>
2016-11-12 17:08:34 -0800

    Added pov() function; err999 sends Pushover mesage

--- direct.php
+++ direct.php
@@ -4,13 +4,14 @@ $test = \filter_input(\INPUT_GET, 'test');
 $do = \filter_input(\INPUT_GET, 'do');
 $path = '../' . (($test) ? 'test' : 'pat') . 'list/';
 $file = $path . 'change.xml';
+$sysop = 'ussgHSjm3HpytUncU6Sg1jimxj7TiJ';
 if ($do=='root') {
     $xml = new SimpleXMLElement('<root />');
     $xml->asXML($file);
     echo 'root';
     exit;
 }
-if ($do=='add') {
+if ($do=='lorem') {
     $xml = (simplexml_load_file($file)) ?: new SimpleXMLElement('<root />');
     $node = $xml[0]->addChild('node');
         $node[0]->addAttribute('MRN', uniqid());
@@ -51,6 +52,34 @@ if ($do=='full') {
     echo $xml->saveXML();
     exit;
 }
+if ($do=='err999') {
+    // bad error: all currlists failed
+    pov($sysop,'EPIC FAIL');
+    exit;
+}
+if ($do=='log') {
+    $log = file_get_contents($path . 'logs/' . date('Ym') . '.log');
+    echo "<font size='2' face='Arial'>";
+    echo nl2br($log);
+    exit;
+}
 else {
     echo 'NULL';
+}
+
+function pov($user,$msg) {
+    curl_setopt_array($ch = curl_init(), array(
+        CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+        CURLOPT_POSTFIELDS => array(
+            "token" => "ab9z9qxvkakctdhamf486ice9r89dv",
+            "user" => $user,
+            "title" => "CHIPOTLE",
+            "message" => $msg,
+            "sound" => "echo"
+        ),
+        CURLOPT_SAFE_UPLOAD => true,
+    ));
+    $ret = curl_exec($ch);
+    curl_close($ch);
+    return $ret;
 }
